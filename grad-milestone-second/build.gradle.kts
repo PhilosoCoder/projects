cc6f87cb35a979310fbd93c786e898ed33d13eb3
Gabor Nagy <gabor.nagy13@tesco.com>
2024-02-23 07:52:47 +0100

    Refactored gradle properties.

--- grad-milestone-second/build.gradle.kts
+++ grad-milestone-second/build.gradle.kts
@@ -11,10 +11,9 @@ java {
 
 repositories {
     mavenCentral()
-    gradlePluginPortal()
 }
 
-val developmentOnly by configurations.creating
+val developmentOnly: Configuration by configurations.creating
 configurations {
     runtimeClasspath {
         extendsFrom(developmentOnly)
@@ -22,28 +21,38 @@ configurations {
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.2"))
+    val springBootVersion = "3.2.3"
+    val junitBomVersion = "5.9.1"
+    val lombokVersion = "1.18.30"
+    val postgresqlVersion = "42.2.27"
+    val thymeleafVersion = "3.1.2.RELEASE"
+    val springBootTestcontainersVersion = "1.19.4"
+
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     implementation("org.springframework.boot:spring-boot-starter-web")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.flywaydb:flyway-core")
     implementation("org.springframework.boot:spring-boot-starter-data-couchbase")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    //implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-web-services")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-    implementation("org.postgresql:postgresql:42.1.4")
-    implementation("org.thymeleaf:thymeleaf:3.1.2.RELEASE")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
+    implementation("org.thymeleaf:thymeleaf:$thymeleafVersion")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers:1.19.4")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers:$springBootTestcontainersVersion")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.testcontainers:couchbase")
     testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql:1.19.4")
+    testImplementation("org.testcontainers:postgresql:$springBootTestcontainersVersion")
+
+    testImplementation(platform("org.junit:junit-bom:$junitBomVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    //implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 tasks {
